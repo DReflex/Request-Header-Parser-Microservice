1eca9a5a4a044b73651840459b74c0e229c78edd
System <none>
2017-03-06 18:18:13 +0000

    ⚓️👟 Updated with Gomix

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var useragent = require('useragent');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+ var headers = request.headers;
+  
+ var agent = useragent.parse(request.headers['user-agent']);
+ var software = agent.os.toString();
+  
+  var connection = headers['x-forwarded-for'].split(',')
+  var ip = connection[0]
+
+  var language = headers['accept-language'].split(',')[0];
+ 
+
+  
+  
+  response.json({
+    software: software,
+    language: language,
+    ip: ip
+  })
+    
+  
+});
+        
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
